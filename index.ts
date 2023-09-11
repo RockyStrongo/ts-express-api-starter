b99200240688be718368e5366924cab64f4cf550
root <root@DESKTOP-K6G9NKE.localdomain>
2023-09-11 18:00:03 +0200

    cors

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import errorHandler from './src/middlewares/errorHandler';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
 import notFound from './src/middlewares/notFound';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -24,6 +25,9 @@ app.use(limiter);
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json({ limit: '50mb' }));
+
+//to do - when in prod, limit domains allowed by cors config
+app.use(cors());
 
 app.use(router);
 app.use(errorHandler);
